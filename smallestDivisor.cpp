16ab728e2073b8ee4cc1830f23ea8225075db0b6
ChimichangasBoi <91523145+ChimichangasBoi@users.noreply.github.com>
2023-08-17 01:43:11 +0530

    Create smallestDivisor.cpp

---
+++ smallestDivisor.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	
+	int n,i;
+	cin>>n;
+	
+	for(i=2;i<=n;i++)
+	{
+		if(n%i==0)
+		{
+			cout<<"smallest divisor: "<<i;
+			
+			break;
+		}
+	}
+	
+return 0;
+
+}
